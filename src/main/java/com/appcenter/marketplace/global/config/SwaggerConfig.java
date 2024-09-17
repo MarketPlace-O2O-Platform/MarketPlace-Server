611eab5828e11ae40f939ef567cd042a39c672e0
82everywin <82everywin@gmail.com>
2024-09-17 23:00:39 +0900

    [Env] JpaConfig 설정

---
+++ src/main/java/com/appcenter/marketplace/global/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.appcenter.marketplace.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("MarketPlace API")
+                .description("MarketPlace API 설명서")
+                .version("1.0.0");
+    }
+}
