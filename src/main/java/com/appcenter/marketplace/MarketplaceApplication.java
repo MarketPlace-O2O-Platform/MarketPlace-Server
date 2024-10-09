b749970f40a48379e8357959729721c91bd2efac
82everywin <82everywin@gmail.com>
2024-10-09 12:26:01 +0900

    [Feat] Swagger HTTPS 처리

--- src/main/java/com/appcenter/marketplace/MarketplaceApplication.java
+++ src/main/java/com/appcenter/marketplace/MarketplaceApplication.java
@@ -1,8 +1,11 @@
 package com.appcenter.marketplace;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://marketplace.inuappcenter.kr", description = "기본 서버 주소")})
 @SpringBootApplication
 public class MarketplaceApplication {
 
