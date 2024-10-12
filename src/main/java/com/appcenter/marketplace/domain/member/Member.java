111a54e4fa6f61e461835c1ccfca19b996edd0a4
82everywin <82everywin@gmail.com>
2024-10-13 01:03:18 +0900

    [Feat] Member 테이블명 설정, 빌더 패턴

--- src/main/java/com/appcenter/marketplace/domain/member/Member.java
+++ src/main/java/com/appcenter/marketplace/domain/member/Member.java
@@ -1,13 +1,16 @@
 package com.appcenter.marketplace.domain.member;
 
 import com.appcenter.marketplace.global.common.BaseEntity;
-import jakarta.persistence.Access;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
+@Table(name = "member")
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -15,5 +18,10 @@ public class Member extends BaseEntity {
 
     @Id
     private Long id;
+
+    @Builder
+    public Member (Long id){
+        this.id = id;
+    }
 
 }
