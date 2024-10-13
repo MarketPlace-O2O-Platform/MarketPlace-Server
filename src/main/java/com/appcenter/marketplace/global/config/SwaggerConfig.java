25f01692d32a523ae1250bd1f67584b8ce355dc2
82everywin <82everywin@gmail.com>
2024-10-13 22:05:57 +0900

    [Issue] Swagger CORS 에러 해결

--- src/main/java/com/appcenter/marketplace/global/config/SwaggerConfig.java
+++ src/main/java/com/appcenter/marketplace/global/config/SwaggerConfig.java
@@ -3,15 +3,23 @@ package com.appcenter.marketplace.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+
+
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI openAPI() {
+        Server server = new Server();
+        server.setUrl("https://marketplace.inuappcenter.kr");
         return new OpenAPI()
                 .components(new Components())
+                .addServersItem(server)
                 .info(apiInfo());
     }
 
