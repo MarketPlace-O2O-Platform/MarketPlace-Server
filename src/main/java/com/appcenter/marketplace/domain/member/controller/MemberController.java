177536a8199c71119d6c76619844a5e51ccd2c94
82everywin <82everywin@gmail.com>
2024-10-13 13:13:02 +0900

    [Docs] Swagger 문서화

--- src/main/java/com/appcenter/marketplace/domain/member/controller/MemberController.java
+++ src/main/java/com/appcenter/marketplace/domain/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.appcenter.marketplace.domain.member.dto.req.MemberLoginReqDto;
 import com.appcenter.marketplace.domain.member.dto.res.MemberLoginResDto;
 import com.appcenter.marketplace.domain.member.service.MemberService;
 import com.appcenter.marketplace.global.common.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "학생 로그인", description = "포털 시스템 정보 (학번, 비밀번호)로 로그인합니다.")
     @PostMapping
     public ResponseEntity<CommonResponse<MemberLoginResDto>> loginMember(@RequestBody MemberLoginReqDto memberLoginReqDto) {
         return ResponseEntity.status(MEMBER_LOGIN_SUCCESS.getStatus())
