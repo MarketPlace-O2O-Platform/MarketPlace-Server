4d39127035ce9372c34a1acead23cde225838d8c
82everywin <82everywin@gmail.com>
2024-10-14 13:37:27 +0900

    [Refactor] Swagger 서버 URL 프로퍼티 파일 분리 - #25

--- src/main/java/com/appcenter/marketplace/global/config/SwaggerConfig.java
+++ src/main/java/com/appcenter/marketplace/global/config/SwaggerConfig.java
@@ -4,19 +4,22 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
 
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.server-url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
         Server server = new Server();
-        server.setUrl("https://marketplace.inuappcenter.kr");
+        server.setUrl(serverUrl);
         return new OpenAPI()
                 .components(new Components())
                 .addServersItem(server)
