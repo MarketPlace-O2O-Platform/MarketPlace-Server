a320ed36805e6dfc37415b036e43c8e42fd55571
82everywin <82everywin@gmail.com>
2024-11-03 17:28:08 +0900

    [Issue] QueryDSL Bean등록

--- src/main/java/com/appcenter/marketplace/global/config/QuerydslConfig.java
+++ src/main/java/com/appcenter/marketplace/global/config/QuerydslConfig.java
@@ -2,14 +2,18 @@ package com.appcenter.marketplace.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class QuerydslConfig {
+
     private final EntityManager entityManager;
 
+    @Bean
     public JPAQueryFactory queryFactory() {
         return new JPAQueryFactory(entityManager);
     }
