a6be10eff586306e985c314263ce9ab5ae6995f7
82everywin <82everywin@gmail.com>
2024-11-05 20:46:08 +0900

    [Refactor] MemberCouponResDto 삭제 -> IssuedMemberCouponResDto 로 통합

--- src/main/java/com/appcenter/marketplace/domain/member_coupon/dto/res/IssuedMemberCouponResDto.java
+++ src/main/java/com/appcenter/marketplace/domain/member_coupon/dto/res/IssuedMemberCouponResDto.java
@@ -1,6 +1,7 @@
 package com.appcenter.marketplace.domain.member_coupon.dto.res;
 
 import com.appcenter.marketplace.domain.member_coupon.MemberCoupon;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,14 +9,17 @@ import java.time.LocalDateTime;
 
 @Getter
 public class IssuedMemberCouponResDto {
+    private final Long memberCouponId;
     private final Long couponId;
     private final String couponName;
     private final String description;
     private final LocalDateTime deadLine;
     private final Boolean used;
 
+    @QueryProjection
     @Builder
-    private IssuedMemberCouponResDto(Long couponId, String couponName, String description, LocalDateTime deadLine, Boolean used) {
+    public IssuedMemberCouponResDto(Long memberCouponId, Long couponId, String couponName, String description, LocalDateTime deadLine, Boolean used) {
+        this.memberCouponId = memberCouponId;
         this.couponId = couponId;
         this.couponName = couponName;
         this.description = description;
@@ -23,8 +27,9 @@ public class IssuedMemberCouponResDto {
         this.used = used;
     }
 
-    public static IssuedMemberCouponResDto toDto(MemberCoupon memberCoupon) {
+    public static IssuedMemberCouponResDto toDto(MemberCoupon memberCoupon){
         return IssuedMemberCouponResDto.builder()
+                .memberCouponId(memberCoupon.getId())
                 .couponId(memberCoupon.getId())
                 .couponName(memberCoupon.getCoupon().getName())
                 .description(memberCoupon.getCoupon().getDescription())
@@ -32,5 +37,4 @@ public class IssuedMemberCouponResDto {
                 .used(memberCoupon.getIsUsed())
                 .build();
     }
-
 }
