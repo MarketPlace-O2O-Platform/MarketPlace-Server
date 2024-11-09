92d480d35ba75fd719b67a9285cde7f4aa3cd8a5
kangwook1 <rkddnr2078@naver.com>
2024-11-09 16:56:27 +0900

    [Feat] 찜한 매장 리스트 조회 서비스 레이어 구현 - #46

--- src/main/java/com/appcenter/marketplace/domain/favorite/service/FavoriteService.java
+++ src/main/java/com/appcenter/marketplace/domain/favorite/service/FavoriteService.java
@@ -1,6 +1,10 @@
 package com.appcenter.marketplace.domain.favorite.service;
 
+import com.appcenter.marketplace.domain.market.dto.res.MarketPageResDto;
+
 public interface FavoriteService {
 
     void createOrDeleteFavorite(Long memberId, Long marketId);
+
+    MarketPageResDto getFavoriteMarketPage(Long memberId, Long marketId, Integer size);
 }
