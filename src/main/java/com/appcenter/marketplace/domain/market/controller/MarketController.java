9a470d4544af3039c0fa35ef5c44f95ef766f74c
kangwook1 <rkddnr2078@naver.com>
2024-11-29 16:48:07 +0900

    [Refactor] 매장 조호 시 page size 5-> 10으로 통일 - #70

--- src/main/java/com/appcenter/marketplace/domain/market/controller/MarketController.java
+++ src/main/java/com/appcenter/marketplace/domain/market/controller/MarketController.java
@@ -35,7 +35,7 @@ public class MarketController {
 
     // size가 null로 들어올 시 기본 값을 지정해주기 위해 integer로 값을 받아온다.
     @Operation(summary = "전체 매장 조회",
-            description = "처음 요청 시, pageSize만 필요합니다. 기본값은 5입니다. <br>" +
+            description = "처음 요청 시, pageSize만 필요합니다. 기본값은 10입니다. <br>" +
                     "카테고리 별 매장을 조회하시려면 category도 필요합니다. <br>" +
                     "최신순으로 보여줍니다.")
     @GetMapping
@@ -44,7 +44,7 @@ public class MarketController {
             @Parameter(description = "페이지의 마지막 marketId")
             @RequestParam(required = false, name = "lastPageIndex") Long marketId,
             @RequestParam(required = false, name = "category") String major,
-            @RequestParam(defaultValue = "5", name = "pageSize") Integer size)
+            @RequestParam(defaultValue = "10", name = "pageSize") Integer size)
     {
         return ResponseEntity
                 .ok(CommonResponse.from(MARKET_FOUND.getMessage()
@@ -53,7 +53,7 @@ public class MarketController {
 
     @Operation(summary = "자신이 찜한 매장 조회",
             description = "자신이 찜한 매장 정보 리스트를 반환합니다. <br>" +
-                    "처음 요청 시엔 pageSize만 필요합니다.기본값은 5입니다. <br>" +
+                    "처음 요청 시엔 pageSize만 필요합니다.기본값은 10입니다. <br>" +
                     "JWT 구현 전 까지는 memberId를 추가해야합니다. <br>" +
                     "최신 찜 순으로 보여줍니다.")
     @GetMapping("/my-favorite")
@@ -61,7 +61,7 @@ public class MarketController {
             @RequestParam Long memberId,
             @Parameter(description = "페이지의 마지막 favoriteModifiedAt (e.g. 2024-11-25 11:19:26.378948 )")
             @RequestParam(required = false, name = "lastModifiedAt") LocalDateTime lastModifiedAt,
-            @RequestParam(defaultValue = "5", name = "pageSize") Integer size) {
+            @RequestParam(defaultValue = "10", name = "pageSize") Integer size) {
         return ResponseEntity
                 .ok(CommonResponse.from(MARKET_FOUND.getMessage()
                         ,marketService.getMyFavoriteMarketPage(memberId,lastModifiedAt,size)));
@@ -69,14 +69,14 @@ public class MarketController {
 
     @Operation(summary = "찜 수가 가장 많은 매장 더보기 조회",
             description = "사용자들이 가장 많이 찜한 매장 정보 리스트를 반환합니다. <br>" +
-                    "처음 요청 시엔 pageSize만 필요합니다. 기본값은 5입니다. <br>" +
+                    "처음 요청 시엔 pageSize만 필요합니다. 기본값은 10입니다. <br>" +
                     "찜 수가 가장 많은 순으로 보여줍니다.")
     @GetMapping("/favorite")
     public ResponseEntity<CommonResponse<MarketPageRes<FavoriteMarketRes>>> getTopFavoriteMarketList(
             @RequestParam Long memberId,
             @Parameter(description = "페이지의 마지막 favoriteCount")
             @RequestParam(required = false, name = "lastFavoriteCount") Long count,
-            @RequestParam(defaultValue = "5", name = "pageSize") Integer size) {
+            @RequestParam(defaultValue = "10", name = "pageSize") Integer size) {
         return ResponseEntity
                 .ok(CommonResponse.from(MARKET_FOUND.getMessage()
                         ,marketService.getFavoriteMarketPage(memberId,count,size)));
@@ -84,10 +84,10 @@ public class MarketController {
 
     @Operation(summary = "찜 수가 가장 많은 매장 TOP 조회",
             description = "사용자들이 가장 많이 찜한 매장 Top을 반환합니다. <br>" +
-                    "size의 기본값은 5입니다.")
+                    "size의 기본값은 10입니다.")
     @GetMapping("/top-favorite")
     public ResponseEntity<CommonResponse<List<TopFavoriteMarketRes>>> getTopFavoriteMarketList(
-            @RequestParam(defaultValue = "5", name = "size") Integer size) {
+            @RequestParam(defaultValue = "10", name = "size") Integer size) {
         return ResponseEntity
                 .ok(CommonResponse.from(MARKET_FOUND.getMessage()
                         ,marketService.getTopFavoriteMarkets(size)));
