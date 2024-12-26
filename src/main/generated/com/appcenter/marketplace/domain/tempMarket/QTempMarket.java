1439368e11d99ec64ef82bfa653d75a833a82096
82everywin <82everywin@gmail.com>
2024-12-27 01:46:21 +0900

    [Feat] 임시 매장 테이블 생성 - #82

---
+++ src/main/generated/com/appcenter/marketplace/domain/tempMarket/QTempMarket.java
@@ -0,0 +1,67 @@
+package com.appcenter.marketplace.domain.tempMarket;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QTempMarket is a Querydsl query type for TempMarket
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QTempMarket extends EntityPathBase<TempMarket> {
+
+    private static final long serialVersionUID = 1836241580L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QTempMarket tempMarket = new QTempMarket("tempMarket");
+
+    public final com.appcenter.marketplace.global.common.QBaseEntity _super = new com.appcenter.marketplace.global.common.QBaseEntity(this);
+
+    public final com.appcenter.marketplace.domain.category.QCategory category;
+
+    public final NumberPath<Long> cheerCount = createNumber("cheerCount", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isHidden = createBoolean("isHidden");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final StringPath name = createString("name");
+
+    public final StringPath thumbnail = createString("thumbnail");
+
+    public QTempMarket(String variable) {
+        this(TempMarket.class, forVariable(variable), INITS);
+    }
+
+    public QTempMarket(Path<? extends TempMarket> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QTempMarket(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QTempMarket(PathMetadata metadata, PathInits inits) {
+        this(TempMarket.class, metadata, inits);
+    }
+
+    public QTempMarket(Class<? extends TempMarket> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.category = inits.isInitialized("category") ? new com.appcenter.marketplace.domain.category.QCategory(forProperty("category")) : null;
+    }
+
+}
+
