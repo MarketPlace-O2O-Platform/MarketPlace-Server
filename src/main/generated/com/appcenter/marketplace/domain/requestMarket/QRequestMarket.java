c8649258e93f37a1ae574463cfb820d3c76785cd
82everywin <82everywin@gmail.com>
2024-12-27 01:46:55 +0900

    [Feat] 요청 매장 테이블 생성 - #82

---
+++ src/main/generated/com/appcenter/marketplace/domain/requestMarket/QRequestMarket.java
@@ -0,0 +1,43 @@
+package com.appcenter.marketplace.domain.requestMarket;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QRequestMarket is a Querydsl query type for RequestMarket
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QRequestMarket extends EntityPathBase<RequestMarket> {
+
+    private static final long serialVersionUID = 795529702L;
+
+    public static final QRequestMarket requestMarket = new QRequestMarket("requestMarket");
+
+    public final StringPath address = createString("address");
+
+    public final NumberPath<Long> count = createNumber("count", Long.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public QRequestMarket(String variable) {
+        super(RequestMarket.class, forVariable(variable));
+    }
+
+    public QRequestMarket(Path<? extends RequestMarket> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QRequestMarket(PathMetadata metadata) {
+        super(RequestMarket.class, metadata);
+    }
+
+}
+
