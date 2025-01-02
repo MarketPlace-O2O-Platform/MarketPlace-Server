b9188660b44e78e37d2e996da0715e75357a3be8
82everywin <82everywin@gmail.com>
2025-01-02 17:01:39 +0900

    [Feat] 임시매장 Entity 컬럼추가 + 레포지토리 - #87

--- src/main/java/com/appcenter/marketplace/domain/tempMarket/TempMarket.java
+++ src/main/java/com/appcenter/marketplace/domain/tempMarket/TempMarket.java
@@ -5,6 +5,7 @@ import com.appcenter.marketplace.domain.category.Category;
 import com.appcenter.marketplace.global.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,7 +27,13 @@ public class TempMarket extends BaseEntity {
     private String name;
 
     @Column(nullable = true)
+    private String description;
+
+    @Column(nullable = true)
     private String thumbnail;
+
+    @Column(nullable = true)
+    private String address;
 
     @Column(name = "cheer_count", nullable = false)
     private Long cheerCount;
@@ -34,10 +41,13 @@ public class TempMarket extends BaseEntity {
     @Column(name = "is_hidden", nullable = false)
     private Boolean isHidden;
 
-    public TempMarket(Category category, String name, String thumbnail, Long cheerCount, Boolean isHidden) {
+    @Builder
+    public TempMarket(Category category, String name, String description, String thumbnail, String address, Long cheerCount, Boolean isHidden) {
         this.category = category;
         this.name = name;
+        this.description = description;
         this.thumbnail = thumbnail;
+        this.address = address;
         this.cheerCount = cheerCount;
         this.isHidden = isHidden;
     }
---
+++ src/main/java/com/appcenter/marketplace/domain/tempMarket/repository/TempMarketRepository.java
@@ -0,0 +1,8 @@
+package com.appcenter.marketplace.domain.tempMarket.repository;
+
+import com.appcenter.marketplace.domain.tempMarket.TempMarket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TempMarketRepository extends JpaRepository<TempMarket, Long> {
+    Boolean existsByName(String name);
+}
