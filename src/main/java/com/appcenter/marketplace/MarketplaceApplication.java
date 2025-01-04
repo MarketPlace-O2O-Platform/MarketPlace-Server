219f0508d58ffc223b99605aef7b142e92327b5b
82everywin <82everywin@gmail.com>
2025-01-04 22:50:30 +0900

    [Env] 스케줄링 활성화 어노테이션 - #90

--- src/main/java/com/appcenter/marketplace/MarketplaceApplication.java
+++ src/main/java/com/appcenter/marketplace/MarketplaceApplication.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@OpenAPIDefinition(servers = {@Server(url = "https://marketplace.inuappcenter.kr", description = "기본 서버 주소")})
+@EnableScheduling // 스케줄링 활성화
 @SpringBootApplication
 public class MarketplaceApplication {
 
