2db998fc0e700b62defcf331b6946b09f552763d
82everywin <82everywin@gmail.com>
2025-01-13 22:22:22 +0900

    [Refactor] boolean->Boolean 변경

--- src/main/java/com/appcenter/marketplace/domain/market/dto/res/TopFavoriteMarketRes.java
+++ src/main/java/com/appcenter/marketplace/domain/market/dto/res/TopFavoriteMarketRes.java
@@ -8,10 +8,10 @@ public class TopFavoriteMarketRes {
     private final Long marketId;
     private final String marketName;
     private final String thumbnail;
-    private final boolean isFavorite;
+    private final Boolean isFavorite;
 
     @QueryProjection
-    public TopFavoriteMarketRes(Long marketId, String marketName, String thumbnail, boolean isFavorite) {
+    public TopFavoriteMarketRes(Long marketId, String marketName, String thumbnail, Boolean isFavorite) {
         this.marketId = marketId;
         this.marketName = marketName;
         this.thumbnail = thumbnail;
