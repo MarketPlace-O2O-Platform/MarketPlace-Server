340918c82be7689f99998129caf4fd9297961cab
82everywin <82everywin@gmail.com>
2025-01-29 20:47:41 +0900

    [Refactor] 만료 스케줄러 작성

---
+++ src/main/java/com/appcenter/marketplace/global/scheduler/MemberCouponExpireScheduler.java
@@ -0,0 +1,25 @@
+package com.appcenter.marketplace.global.scheduler;
+
+import com.appcenter.marketplace.domain.member_coupon.service.MemberCouponService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class MemberCouponExpireScheduler {
+    private final MemberCouponService memberCouponService;
+
+    // 받은 쿠폰이 3일뒤 사용 안했다면 만료
+    @Scheduled(cron = "${schedule.cron.run-daily}", zone="Asia/Seoul")
+    public void check3DaysCoupons() {
+        try{
+            memberCouponService.check3DaysCoupons();
+        }catch(Exception e){
+            log.error("MemberCouponExpireScheduler: {} 에러 발생", e.getMessage());
+        }
+    }
+
+}
