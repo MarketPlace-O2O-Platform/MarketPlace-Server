f647d43752778dd75c71488f1d82e4cc1442d5ce
82everywin <82everywin@gmail.com>
2025-01-29 20:53:21 +0900

    [Refactor] 공감권 repository-> service로 의존성 주입 변경

--- src/main/java/com/appcenter/marketplace/global/scheduler/CheerTicketScheduler.java
+++ src/main/java/com/appcenter/marketplace/global/scheduler/CheerTicketScheduler.java
@@ -1,29 +1,27 @@
 package com.appcenter.marketplace.global.scheduler;
 
-import com.appcenter.marketplace.domain.member.repository.MemberRepository;
+import com.appcenter.marketplace.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class CheerTicketScheduler {
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
-    @Scheduled(cron = "${schedule.cheerTicket.cron}", zone= "Asia/Seoul")
-    @Transactional
+    @Scheduled(cron = "${schedule.cron.run-daily}", zone= "Asia/Seoul")
     public void reChargeCheerTicket() {
         log.info("CheerTicketScheduler.reChargeCheerTicket: 매일 자정 회원 공감권 1개씩 충전 ");
         try{
-            long resetCheerTickets = memberRepository.resetCheerTickets();
+            long resetCheerTickets = memberService.resetCheerTickets();
             log.info("CheerTicketScheduler.reChargeCheerTicket: {}명 충전 완료", resetCheerTickets);
 
         }catch(Exception e){
-            log.error("CheerTicketScheduler.reChargeCheerTicket: 에러 발생");
+            log.error("CheerTicketScheduler.reChargeCheerTicket: 에러 발생 - {}", e.getMessage());
         }
     }
 }
