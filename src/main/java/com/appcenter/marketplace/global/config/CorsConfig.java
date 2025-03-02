d6a9eabf33f7381cc254c63136505a10566810af
82everywin <82everywin@gmail.com>
2025-03-03 03:02:05 +0900

    [Refactor] url value 처리

--- src/main/java/com/appcenter/marketplace/global/config/CorsConfig.java
+++ src/main/java/com/appcenter/marketplace/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.appcenter.marketplace.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${cors.web.url}")
+    private String webUrl;
+
+    @Value("${cors.server.url}")
+    private String serverUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173","https://marketplace.inuappcenter.kr")
+                .allowedOrigins(webUrl,serverUrl)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
