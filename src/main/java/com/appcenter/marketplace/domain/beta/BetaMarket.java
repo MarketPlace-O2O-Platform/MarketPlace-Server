fba6e9667a480ef2f795c3c19aa17b7950d6422f
kangwook1 <rkddnr2078@naver.com>
2025-03-05 19:30:45 +0900

    [beta] 베타 버전 매장,발급 쿠폰 엔티티 구현 - #117

---
+++ src/main/java/com/appcenter/marketplace/domain/beta/BetaMarket.java
@@ -0,0 +1,55 @@
+package com.appcenter.marketplace.domain.beta;
+
+
+import com.appcenter.marketplace.domain.category.Category;
+import com.appcenter.marketplace.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "beta_market")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BetaMarket extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="market_name",nullable = false)
+    private String marketName;
+
+    @Column(name="coupon_name",nullable = false)
+    private String couponName;
+
+    @Column(name="coupon_detail",nullable = false)
+    private String couponDetail;
+
+    @Column(name = "dead_line",nullable = false)
+    private LocalDateTime deadLine;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private String image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Builder
+    public BetaMarket(String marketName, String couponName, String couponDetail, LocalDateTime deadLine, Integer stock, Category category) {
+        this.marketName = marketName;
+        this.couponName = couponName;
+        this.couponDetail = couponDetail;
+        this.deadLine = deadLine;
+        this.stock = stock;
+        this.category = category;
+    }
+}
